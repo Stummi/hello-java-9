4446f3fc382ccdc92d5ae2860ca7a7d93e3b2e02
Michael Stummvoll <michael.stummvoll@autinity.de>
2017-02-06 14:13:22 +0100

    Initial Commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,1 @@
+module org.stummi.hello9 {}
---
+++ src/main/java/org/stummi/hello9/Hello9.java
@@ -0,0 +1,7 @@
+package org.stummi.hello9;
+
+class Hello9 {
+    public static void main(String... args) {
+        System.out.println("Hello, Java9!");
+    }
+}
